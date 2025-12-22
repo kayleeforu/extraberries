c5786876dd30df27ca520214d9a5e95966647843
akkay <ak.kayleeforu@gmail.com>
2025-12-22 05:02:20 +0200

    Initial commit

---
+++ src/main/java/com/akkay/extraberries/ExtraBerries.java
@@ -0,0 +1,16 @@
+package com.akkay.extraberries;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExtraBerries implements ModInitializer {
+	public static final String MOD_ID = "extra-berries";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/akkay/extraberries/ExtraBerriesClient.java
@@ -0,0 +1,10 @@
+package com.akkay.extraberries;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraBerriesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
